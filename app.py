2e7d254c5bd59df14b2ad0358db3b383c7228f77
aceclub01 <acelee@dbs.com>
2025-03-26 20:10:15 +0800

    .

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import os
 
 app = Flask(__name__)
 
@@ -21,5 +22,6 @@ def index():
     slides = parse_slides()
     return render_template('index.html', slides=slides)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))  # Get the port from environment variable or default to 10000
+    app.run(host="0.0.0.0", port=port)
