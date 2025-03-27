c0bb3d2a32afec6b30466a26e6587e38cb8674c0
aceclub01 <acelee@dbs.com>
2025-03-27 22:01:57 +0800

    .

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os
+from app import app
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -21,5 +23,7 @@ def index():
     slides = parse_slides()
     return render_template('index.html', slides=slides)
 
+# if __name__ == "__main__":
+#     app.run(debug=True, host='0.0.0.0', port=5000)  # Make sure to listen on all IP addresses
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=5000)  # Make sure to listen on all IP addresses
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
